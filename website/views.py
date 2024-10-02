6456a43385289ff21936542fb2eeb1da16ec5601
princebeersingh <princebeersingh@gmail.com>
2024-10-02 15:10:03 +0530

    some changes and workdone on login stuff

--- website/views.py
+++ website/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
+
+# check if user is logging in
 def home(request):
+  if request.method == "POST":
+    username =request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request,username=username,password=password)
   return render(request, 'home.html')
+
+
+
+# def login_user(request):
+  pass
+
+
+def logout_user(request):
+  pass
